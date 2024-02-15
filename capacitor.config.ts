4c3b90acb1ee875a579faf3d8d83f3f4496109af
Jared Youtsey <github@fivedice.net>
2024-02-15 09:56:30 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'videojs-playback-rates-flicker',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
